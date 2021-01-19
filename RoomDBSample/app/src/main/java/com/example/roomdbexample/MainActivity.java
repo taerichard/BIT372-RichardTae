d9d298d8ca31481babf24236b2e90a97f4a4f78b
Richard Tae <taerichard@gmail.com>
2021-01-18 23:08:29 -0800

    created event obj

---
+++ RoomDBSample/app/src/main/java/com/example/roomdbexample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.roomdbexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView eventList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, CreateEventActivity.class);
+        FloatingActionButton fab = findViewById(R.id.add_event_btn);
+        fab.setOnClickListener((view) -> startActivity(intent));
+        eventList = findViewById(R.id.event_list);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
