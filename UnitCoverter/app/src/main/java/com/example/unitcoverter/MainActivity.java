ae960185056d75423c82dac541ec39612d640742
Richard Tae <taerichard@gmail.com>
2021-02-17 00:45:53 -0800

    created spinner with values responding to UI, working on binding data

--- UnitCoverter/app/src/main/java/com/example/unitcoverter/MainActivity.java
+++ UnitCoverter/app/src/main/java/com/example/unitcoverter/MainActivity.java
@@ -4,21 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button fbtn = findViewById(R.id.convert_btn);
-        Button poundKgButton = findViewById(R.id.poundToKg_Button);
+        // spinner
+        Spinner spinner = findViewById(R.id.spinner);
+        // array adapter with list of conversations
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.conversations_array, R.layout.support_simple_spinner_dropdown_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
 
-        EditText poundInput = findViewById(R.id.pound_input);
+        // item selected listener for spinner
+        spinner.setOnItemSelectedListener(this);
+
+        Button fbtn = findViewById(R.id.convert_btn);
+
         TextView kgText = findViewById(R.id.kg_text);
 
         EditText finput = findViewById(R.id.fahrenheit_input);
@@ -29,15 +41,31 @@ public class MainActivity extends AppCompatActivity {
             double celcius = Converter.toCelcius(Float.parseFloat(finput.getText().toString()));
             ctxt.setText(String.format("%.2f ºC", celcius));
         });
+    }
 
-        poundKgButton.setOnClickListener((v) ->{
-            if(poundInput.getText().length() == 0){
-                return;
-            }
-            double kg = Converter.poundToKg(Double.parseDouble(poundInput.getText().toString()));
-            Log.i("INFO", "Value " + kg);
-            kgText.setText(Double.toString(kg));
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        switch(position){
+            case 0 :
+                Log.i("INFO", parent.getItemAtPosition(0).toString());
+                break;
+            case 1 :
+                Log.i("INFO", parent.getItemAtPosition(1).toString());
+                break;
+            case 2 :
+                Log.i("INFO", parent.getItemAtPosition(2).toString());
+                break;
+            case 3 :
+                Log.i("INFO", parent.getItemAtPosition(3).toString());
+                break;
+        }
+    }
 
-        });
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
+
