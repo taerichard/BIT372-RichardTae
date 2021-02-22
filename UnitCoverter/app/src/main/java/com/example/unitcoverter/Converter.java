814f11aaf0f0905412bf142dd2b106fe68bb0345
Richard Tae <taerichard@gmail.com>
2021-02-21 17:47:35 -0800

    f

--- UnitCoverter/app/src/main/java/com/example/unitcoverter/Converter.java
+++ UnitCoverter/app/src/main/java/com/example/unitcoverter/Converter.java
@@ -4,4 +4,7 @@ public class Converter {
     public static float toCelcius(float fah) {
         return (fah - 32) * 5/9;
     }
+    public static float toKilograms(float pounds){ return pounds * (float) 0.45359237; }
+    public static float toFeet(float inches){ return inches/12; }
+    public static float toMl(float ounces) { return ounces * (float) 29.57353; }
 }
